0b9c8a4ebb719d06b775138e3b13183191ebdbeb
Wilker <wilker_a@hotmail.com>
2018-11-07 10:34:34 -0200

    implementação do cadastro de usuários  com foto

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\UsersRequest;
 
 use App\User;
 use App\Role;
+use App\Photo;
 
 class AdminUsersController extends Controller
 {
@@ -29,9 +30,17 @@ class AdminUsersController extends Controller
    
     public function store(UsersRequest $request)
     {
-        //return $request->all();
-        User::create($request->all());
+        $input = $request->all();
 
+        if($file = $request->file('photo_id')){
+            $name = time() . $file->getClientOriginalName();
+            $file->move('images', $name);
+            $photo = Photo::create(['file'=>$name]);
+            $input['photo_id'] = $photo->id;
+        }
+
+        $input['password'] = bcrypt($request->password);
+        User::create($input);
         return redirect('/admin/users');
     }
 
