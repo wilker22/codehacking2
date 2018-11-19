a5bfed4672b13ca5b996ffe44679eb8d061db365
Wilker <wilker_a@hotmail.com>
2018-11-19 18:47:17 -0200

    com o Middleware pronto segunda parte

--- app/User.php
+++ app/User.php
@@ -32,4 +32,19 @@ class User extends Authenticatable
     public function photo(){
         return $this->belongsTo('App\Photo');
     }
+
+    public function setPasswordAttribute($password){
+        if(!empty($password)){
+            $this->attributes['password'] = bcrypt($password);
+        }
+    }
+
+    public function isAdmin()
+    {
+           if($this->role->name = "Administrator"){
+               return true;
+           }else{
+               return false;
+           }
+    }
 }
