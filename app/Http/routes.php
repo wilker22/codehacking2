46641df26bf82b916d23ebc78ffdbef6e605ed4a
Wilker <wilker_a@hotmail.com>
2018-11-19 18:30:53 -0200

    commit com middleware e rotas em grupos

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,4 +27,11 @@ Route::get('/admin', function(){
     return view('admin.index');
 });
 
-Route::resource('admin/users', 'AdminUsersController');
+
+Route::group(['middleware' => 'admin'], function() {
+   
+    Route::resource('admin/users', 'AdminUsersController');
+
+});
+
+
